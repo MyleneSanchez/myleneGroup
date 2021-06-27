896233a99aa02da804a07b9888695956a727a620
MyleneSanchez <55828440+MyleneSanchez@users.noreply.github.com>
2021-06-28 00:13:55 +0800

    Add files via upload

---
+++ createpost.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Create Post</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<?php
+    require('db.php');
+    // When form submitted, insert values into the database.
+    if (isset($_REQUEST['username'])) {
+        // removes backslashes
+        $username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+        $username = mysqli_real_escape_string($con, $username);
+
+        $fullname    = stripslashes($_REQUEST['fullname']);
+        $fullname    = mysqli_real_escape_string($con, $fullname);
+
+        $email    = stripslashes($_REQUEST['email']);
+        $email    = mysqli_real_escape_string($con, $email);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        $create_datetime = date("Y-m-d H:i:s");
+        $query    = "INSERT into `users` (username, fullname, password, email, create_datetime)
+                     VALUES ('$username', '$fullname','" . md5($password) . "', '$email', '$create_datetime')";
+        $result   = mysqli_query($con, $query);
+        if ($result) {
+            echo "<div class='form'>
+                  <h3>You are registered successfully.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a></p>
+                  </div>";
+        } else {
+            echo "<div class='form'>
+                  <h3>Required fields are missing.</h3><br/>
+                  <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
+    <form class="form" action="" method="post">
+        <h1 class="login-title">Create Post</h1>
+        <input type="text" class="login-input" name="fullname" placeholder="Full Name" required />
+        <input type="text" class="login-input" name="username" placeholder="Name of Community Pantry" required />
+        <input type="text" class="login-input" name="email" placeholder="Email Address">
+        <input type="text" class="login-input" name="fullname" placeholder="Street Address" required />
+        <input type="text" class="login-input" name="fullname" placeholder="Barangay" required />
+        <input type="text" class="login-input" name="fullname" placeholder="Municipality/City" required />
+        <input type="text" class="login-input" name="fullname" placeholder="Phone Number" required />
+        <input type="text" class="login-input" name="fullname" placeholder="GCash Number" required />
+        <input type="text" class="login-input" name="fullname" placeholder="Schedule" required />
+        <input type="text" class="login-input" name="fullname" placeholder="More Information"/>
+
+        <input type="submit" name="submit" value="Submit" class="login-button">
+        
+    </form>
+<?php
+    }
+?>
+</body>
+</html>
---
+++ homepage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pantry Finder | Homepage</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">    
+</head>
+<body>
+
+    <header>
+    
+  
+   
+       <!-- <div class="logo">
+       
+   <br> <br>
+            <img src="images/PantryFinder(1).png">
+
+        </div> -->
+
+
+<div class="wrapper">
+
+<ul class="nav-area">
+<li><a href="homepage.php">Home</a></li>
+<li><a href="#">About</a></li>
+<li><a href="#">Go to Map</a></li>
+<li><a href="login.php">Log In</a></li>
+
+</ul>
+</div>
+<div class="welcome-text">
+        <h1> <br> <br> <br>
+Online Community Pantry Mapping <span>and Donation Management System</span></h1>
+<a href="viewpost.php"> See More</a>
+    </div> <br>
+  
+    
+  <br> &nbsp; <p align = "center"> <img src="images/PantryFinder(1).png"> </p>
+
+</header>
+
+
+</body>
+</html>
